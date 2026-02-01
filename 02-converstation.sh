e5fb5ca5d1a7e3fd4e6426f31724bfd04856ebf4
sailokesh <appanasailokesh1112@gmail.com>
2026-02-01 14:46:44 +0000

    converstaion added

---
+++ 02-converstation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#conversation between ram and sam
+
+echo "Ram:: Hi sam , how are you"
+echo "Sam:: fine , what about you"
+echo "Ram:: where are working now"
+echo "Sam:: working in joindevops"
