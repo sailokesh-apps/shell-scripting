033eecb1919053a8924d23a929f6bef24275fd0d
sailokesh <appanasailokesh1112@gmail.com>
2026-02-04 13:54:40 +0000

    colurs.sh

---
+++ 16-coolurs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+
+echo  "$R Hello bro $N, $y I am working in joindevops $N"
--- 17-colours.sh
+++ 17-colours.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
+#colours
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ]; then
-    echo "Please run this script with root user access" | tee -a $LOGS_FILE
+    echo "Please run this script with $R root user access $N" | tee -a $LOGS_FILE
     exit 1
 fi
 
-mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo "$2 ... FAILURE" | tee -a $LOGS_FILE
+        echo "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
     else
-        echo "$2 ... SUCCESS" | tee -a $LOGS_FILE
+        echo "$2 ... $G SUCCESS $Y" | tee -a $LOGS_FILE
     fi
 }
 
@@ -28,6 +34,6 @@ do
         apt install $package -y &>>$LOGS_FILE
         VALIDATE $? "$package installation"
     else
-        echo "$package is already installed skip now"
+        echo "$G $package is already installed skip now $N"
     fi 
 done
